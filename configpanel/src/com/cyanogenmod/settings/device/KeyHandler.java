cf2b3f21f184a6f78df6e023bc52a5fc91f4eed7
Scott Mertz <scott@cyngn.com>
2014-09-08 10:32:41 -0700

    Gestures: Ensure system stays awake for gesture handling
    - Async broadcasts will allow the system to go back to sleep
      before the receiver handles the event.  Acquire a wake lock
      for a brief duration to allow the receiver time to receive
      (and acquire it's own wake lock if needed) the intent.
    
    Change-Id: I0667c321178b2a27dc4bbc869b5576b98e768124

--- configpanel/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ configpanel/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -42,6 +42,8 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final int GESTURE_GTR_SCANCODE = 254;
     private static final int KEY_DOUBLE_TAP = 255;
 
+    private static final int GESTURE_WAKELOCK_DURATION = 3000;
+
     private static final int[] sSupportedGestures = new int[]{
         FLIP_CAMERA_SCANCODE,
         GESTURE_CIRCLE_SCANCODE,
@@ -59,6 +61,7 @@ public class KeyHandler implements DeviceKeyHandler {
     private SensorManager mSensorManager;
     private Sensor mProximitySensor;
     WakeLock mProximityWakeLock;
+    WakeLock mGestureWakeLock;
 
     public KeyHandler(Context context) {
         mContext = context;
@@ -68,6 +71,8 @@ public class KeyHandler implements DeviceKeyHandler {
         mProximitySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         mProximityWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "ProximityWakeLock");
+        mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "GestureWakeLock");
     }
 
     private void ensureKeyguardManager() {
@@ -93,6 +98,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 } else {
                     action = MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA;
                 }
+                mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
                 Intent intent = new Intent(action, null);
                 startActivitySafely(intent);
                 break;
@@ -100,6 +106,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 dispatchMediaKeyWithWakeLockToAudioService(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
                 break;
             case GESTURE_V_SCANCODE:
+                mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
                 AwesomeAction.launchAction(mContext, AwesomeConstant.ACTION_TORCH.value());
                 break;
             case GESTURE_LTR_SCANCODE:
