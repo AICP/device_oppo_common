a6d1ecd71eb3ce17fafae034068bc169f0b9005b
RobbieL811 <robbielancaster811@gmail.com>
2015-05-27 00:51:23 -0400

    Don't break screen off gestures when dex preopting builds
    Change-Id: Ia8b26a285c8f8d5026aa62171fa565411f2de03c
    Signed-off-by: RobbieL811 <robbielancaster811@gmail.com>

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := DeviceHandler
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_DEX_PREOPT := false
 
 include $(BUILD_PACKAGE)
