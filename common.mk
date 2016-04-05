10354f9e6dbb7103f52fdfe2aeb26ec14e29d692
Martinusbe <martinusbe@gmail.com>
2016-04-05 13:20:12 +0200

    remove cm keyhandler
    Change-Id: Ida022a4f7cf9791c578a5ef00c6f2212a891756f

--- common.mk
+++ common.mk
@@ -21,15 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
 	DeviceHandler
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-
-# never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_oppo
