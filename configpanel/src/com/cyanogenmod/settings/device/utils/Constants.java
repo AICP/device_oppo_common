01929e054557c630908a7bd305eb7fafa8f15f7e
SpiritCroc <spiritcroc@gmail.com>
2017-08-24 19:42:26 +0200

    [WIP][TEST] Make notification slider setting kernel independent
    Change-Id: I3cc63e363ccec19b96a76ee4369d3692cbf98402

--- configpanel/src/com/cyanogenmod/settings/device/utils/Constants.java
+++ configpanel/src/com/cyanogenmod/settings/device/utils/Constants.java
@@ -38,9 +38,11 @@ public class Constants {
     public static final String OCLICK_FENCE_KEY = "oclick_fence";
     public static final String OCLICK_DISCONNECT_ALERT_KEY = "oclick_disconnect_alert";
     public static final String BUTTON_SWAP_KEY = "button_swap";
+    /*
     public static final String NOTIF_SLIDER_TOP_KEY = "keycode_top_position";
     public static final String NOTIF_SLIDER_MIDDLE_KEY = "keycode_middle_position";
     public static final String NOTIF_SLIDER_BOTTOM_KEY = "keycode_bottom_position";
+    */
 
     // Proc nodes
     public static final String TOUCH_PAD_NODE = "/proc/touchpad/enable";
@@ -63,25 +65,31 @@ public class Constants {
 
     public static final String[] sButtonPrefKeys = {
         BUTTON_SWAP_KEY,
+    /*
         NOTIF_SLIDER_TOP_KEY,
         NOTIF_SLIDER_MIDDLE_KEY,
         NOTIF_SLIDER_BOTTOM_KEY
+    */
     };
 
     static {
         sBooleanNodePreferenceMap.put(TOUCHPAD_STATE_KEY, TOUCH_PAD_NODE);
         sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, BUTTON_SWAP_NODE);
+        /*
         sStringNodePreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_NODE);
         sStringNodePreferenceMap.put(NOTIF_SLIDER_MIDDLE_KEY, NOTIF_SLIDER_MIDDLE_NODE);
         sStringNodePreferenceMap.put(NOTIF_SLIDER_BOTTOM_KEY, NOTIF_SLIDER_BOTTOM_NODE);
+        */
 
         sNodeDefaultMap.put(TOUCHPAD_STATE_KEY, TOUCH_PAD_DEFAULT);
         sNodeDefaultMap.put(TOUCHPAD_DOUBLETAP_KEY, false);
         sNodeDefaultMap.put(TOUCHPAD_LONGPRESS_KEY, false);
         sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
+        /*
         sNodeDefaultMap.put(NOTIF_SLIDER_TOP_KEY, "601");
         sNodeDefaultMap.put(NOTIF_SLIDER_MIDDLE_KEY, "602");
         sNodeDefaultMap.put(NOTIF_SLIDER_BOTTOM_KEY, "603");
+        */
 
         sNodeDefaultMap.put(OCLICK_FENCE_KEY, true);
         sNodeDefaultMap.put(OCLICK_DISCONNECT_ALERT_KEY, true);
