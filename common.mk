184d6820eeb350f93ae55414dd4465bfc1b74f96
martinusbe <martinusbe@gmail.com>
2017-12-10 09:42:16 +0100

    disable the devicehandler

--- common.mk
+++ common.mk
@@ -18,8 +18,8 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # DevicHandler
-PRODUCT_PACKAGES += \
-	DeviceHandler
+#PRODUCT_PACKAGES += \
+#	DeviceHandler
 
 # Recovery
 PRODUCT_PACKAGES += \
