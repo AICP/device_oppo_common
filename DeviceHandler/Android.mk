d1480341aaebfc864c4adf831d8cd93ff99c2f40
martinusbe <martinusbe@gmail.com>
2018-08-18 23:25:00 +0200

    devicehandler: set LOCAL_PRIVATE_PLATFORM_APIS

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -20,6 +20,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PACKAGE_NAME := DeviceHandler
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_DEX_PREOPT := false
 
