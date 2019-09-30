2c3904173ba3ed2cb1138deb0e09722260e42be7
Ali B <abittin@gmail.com>
2019-09-30 23:39:48 +0300

    oppo-common: remove DeviceHandler for now
    Change-Id: I87babe8d6d7bb3cb80c3b8b8f1535231ac0b2945

--- common.mk
+++ common.mk
@@ -17,11 +17,10 @@
 # overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# DevicHandler
-PRODUCT_PACKAGES += \
-	DeviceHandler
+## DevicHandler
+#PRODUCT_PACKAGES += \
+#	DeviceHandler
 
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_oppo
-
